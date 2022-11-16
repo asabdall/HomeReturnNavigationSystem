93edd6dcfa704ab6047a3e38b5da3ee05c60e815
Abdalla, Ahmed S <asabdall@iu.edu>
2022-11-16 08:50:25 -0500

    Optimizing Printout into a function

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 //Including all of the Header Files
+
 #include <stdio.h>
 #include <string.h>
 #include <sstream>
@@ -6,57 +7,54 @@
 #include "hcsr04.h"
 #include "math.h"
 
-//IO declarations
+//Serial PC USB Declaration
+
 Serial pc(USBTX, USBRX); //PC UART
+
 //Supersonic Sensor Declaration
+
 HCSR04 xsensor(D8,D9);//ECHO Pin=D9, TRIG Pin=D8
 HCSR04 ysensor(D7,D6);//ECHO Pin=D7, TRIG Pin=D6
-//Bluetooth module declaration
+
+//Bluetooth Module declaration
+
 Serial HC06(PTC15, PTC14); //BT TX, RX
-//BOARD LEDs
-DigitalOut RLed(LED1);                      //Onboard Red LED = Shelf Out of Stock
-DigitalOut GLed(LED2);                      //Onboard Green LED = All OK
+
+//Board LED Declaration
+
+DigitalOut RLed(LED1);                      
+DigitalOut GLed(LED2);                      
 DigitalOut BLed(LED3);   
+
 //Instantiate Send and Receive Variables
 char snd[512], rcv[1000]; //send and receive buffer
 //Instantiate Variable for the Size of the Room, and distance from walls in the x and y direction
 int initial_x_row, initial_y_column,x_distance,y_distance,roomwidth, roomlength,current_row,current_column,obstacles;
-stringstream strs;
+void room_size_printout(int printitem,char* printelement);
 int main()
 {
-   //default settings
+    //Program Menu
+
     HC06.printf("-----Home Return Navigation System-----\n");//Printing Title of the Project to the Phone
     HC06.printf("Group Members: Ahmed Abdalla, Reza Soltani, Nujhat Tabassum\n");//Printing Members of the Project to the Phone
+
+    //Asking User for Room Width and Outputting it back to the phone
+
     HC06.printf("Please Enter the Width of the Room in Meters\n");//Asking the user to enter the room width
     HC06.gets(rcv, 1000);//Receiving the room width input from the user's phone
     roomwidth=stoi(rcv);//Setting the input equal to the input from the phone
-    //Outputting Entered room size back to phone
-    strs << roomwidth;
-    string temp_str = strs.str();
-    char* char_type = (char*) temp_str.c_str();
-    HC06.puts("Room Width Is: ");
-    HC06.puts(char_type);
-    HC06.puts(" Meters\n");
-    strs=stringstream();
-    temp_str=string();
-    char_type=(char*)"";
+    room_size_printout(roomwidth, (char*)"Width");
 
-    //
+    //Asking User for Room Length and Outputting it back to the phone
+
     HC06.printf("Please Enter the Length of the Room in Meters\n");//Asking the user to enter the room size
     HC06.gets(rcv, 1000);//Receiving the room size input from the user's phone
     roomlength=stoi(rcv);//Setting the input equal to the input from the phone
-    //Outputting Entered room size back to phone
-    strs << roomlength;
-    temp_str = strs.str();
-    char_type = (char*) temp_str.c_str();
-    HC06.puts("Room Length Is: ");
-    HC06.puts(char_type);
-    HC06.puts(" Meters\n");
-    strs=stringstream();
-    temp_str=string();
-    char_type=(char*)"";
+     room_size_printout(roomlength, (char*)"Length");
     int ishome=0;
+    
     //Prompting User to choose when they have reached their desired home location
+
     while(ishome==0){
     HC06.printf("Please Enter \"HOME\" When you have reached the Desired home Location\n");
     HC06.gets(rcv, 1000);
@@ -64,6 +62,7 @@ int main()
         ishome=1;
     }
     }
+
     //Setting Initial Position
         HC06.printf("Setting Home Location...\n");
         xsensor.start();
@@ -90,22 +89,26 @@ int main()
         current_row=round(x_distance/100);
         current_column=round(y_distance/100);
         //Finding whether the current distance from home is less than the previous distance, and lighting
-        //the corresponding LED color. If closer, Green, same, Blue, farther, Red
+        //the corresponding LED color. If closer, Green, same, Yellow, farther, Red
         if(old_distance>(abs(current_row-initial_x_row)+abs(current_column-initial_y_column))){
             RLed = 0;
-            BLed = 0;
+            BLed = 1;
             GLed = 1;
+            HC06.printf("\nCloser\n");
         }
-        if(old_distance<(abs(current_row-initial_x_row)+abs(current_column-initial_y_column))){
+        else if(old_distance<(abs(current_row-initial_x_row)+abs(current_column-initial_y_column))){
             RLed = 1;
-            BLed = 0;
+            BLed = 1;
             GLed = 0;
+            HC06.printf("\nFarther\n");
         }
-        else{
+        else {
             RLed = 0;
             BLed = 1;
             GLed = 0;
+            HC06.printf("\nSame\n");
         }
+        old_distance=abs(current_row-initial_x_row)+abs(current_column-initial_y_column);
         //Finding the direction towards the home location from the current location
         HC06.puts("Directions to Home:");
         if (current_column<initial_y_column){
@@ -177,4 +180,15 @@ int main()
         HC06.puts("\n\n----------------------------\n\n");
         wait(5);
     }
+}
+void room_size_printout(int printitem,char* printelement){
+    stringstream strs;
+    strs << printitem;
+    string temp_str = strs.str();
+    char* char_type = (char*) temp_str.c_str();
+    HC06.puts("Room ");
+    HC06.puts(printelement);
+    HC06.puts(" Is: ");
+    HC06.puts(char_type);
+    HC06.puts(" Meters\n");
 }
